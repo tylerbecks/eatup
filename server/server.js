4d7e13004f112527b3d99c4766692eb8c360fad4
Tyler Becks <tylerdbecks@gmail.com>
2016-07-15 16:01:12 -0700

    Define database tables and associations

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ var app = express();
 
 require('./config/routes.js')(app, express);
 require('./config/middleware.js')(app, express);
+require('./db/db.js');
 
-
-app.listen(3000,  function() {
+app.listen(3000, function() {
   console.log('listening on ' + 3000);
 });
