96138680783dffc74851ad754194e20973784809
isunnyvirk <isunnyvirk@gmail.com>
2016-07-21 15:37:28 -0700

    map on page

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, hashHistory } from 'react-router';
-// import AwesomeComponent from './AwesomeComponent.jsx';
+
 //Imports all of the different components
-import Home from './components/Home.jsx';
-import SignIn from './components/SignIn.jsx';
-import SignUp from './components/SignUp.jsx';
+// import Home from './components/Home.jsx';
+// import SignIn from './components/SignIn.jsx';
+// import SignUp from './components/SignUp.jsx';
+import myMap from './components/Map.jsx';
 
-class App extends React.Component {
 
-  render () {
+// class App extends React.Component {
 
-    return (
-      //Defines the routes for app and renders different components
-      <Router history={ hashHistory }>
-        <Route path="/" component={Home}></Route>
-        <Route path="SignIn" component={SignIn}></Route>
-        <Route path="SignUp" component={SignUp}></Route>
-      </Router>
-    )
-  }
-}
+//   render () {
 
-const app = document.getElementById('app');
-ReactDOM.render(<App />, app);
+//     return (
+//       //Defines the routes for app and renders different components
+//       <Router history={ hashHistory }>
+//         <Route path="/" component={Home}></Route>
+//         <Route path="SignIn" component={SignIn}></Route>
+//         <Route path="SignUp" component={SignUp}></Route>
+//         <Route path='Map' component={myMap}></Route>
+//       </Router>
+//     )
+//   }
+// }
+
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(myMap),
+    document.getElementById('app')
+  );
+});
--- server/server.js
+++ server/server.js
@@ -3,9 +3,26 @@ var express = require('express');
 var app = express();
 var router = require('./config/routes.js');
 
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('../webpack.config.js');
+var compiler = webpack(webpackConfig);
+
+
 require('./config/middleware.js')(app, express);
 require('./models/db.js');
 app.use(router);
+
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+
 
 app.listen(3000, function() {
   console.log('listening on ' + 3000);
