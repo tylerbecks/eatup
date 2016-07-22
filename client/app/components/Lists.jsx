9d98a232c2ea4f89d34fab2d8cc80441ca805fee
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-07-22 16:44:28 -0700

    Add react components for main components

--- client/app/components/Lists.jsx
+++ client/app/components/Lists.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 class ListOfEatUp extends React.Component {
   constructor(props) {
     super(props);
-<<<<<<< 099368ea08e0e6b7d76cc6a2c1ac6c7340f0d139
   }
 
   render () {
@@ -14,27 +13,6 @@ class ListOfEatUp extends React.Component {
       <div>
       <h1>Hello</h1>
       {resultStuffs}
-=======
-    this.state = {}
-  }
-
-  getdata () {
-    $.ajax({
-      method: 'GET',
-      url: 'localhost:3000/sessions/allSessions'
-
-    })
-  }
-
-  componentWillMount() {
-
-  }
-
-  render () {
-    return (
-      <div>
-      <h1>Hello</h1>
->>>>>>> Begin incorporating jQuery ajax format
       </div>
     )
   }
